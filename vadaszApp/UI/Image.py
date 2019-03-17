5735ac17fbcec000433e72719591e39ed8aaebf5
Ferenc Janky <fecjanky@gmail.com>
2019-03-17 19:34:26 +0100

    apply box filtering to original image to reduce aliasing when resizing window

--- vadaszApp/UI/Image.py
+++ vadaszApp/UI/Image.py
@@ -12,7 +12,7 @@ class Image(Drawable):
         self.path = path
         canvas.attach(self)
         self.keep_aspect_ratio = get_from_dict(kwargs, "keep_aspect_ratio", True)
-        self.original_image = cv2.imread(path)
+        self.original_image = cv2.boxFilter(cv2.imread(path), -1, (5, 5))
         self.original_height, self.original_width, z = self.original_image.shape
         self.portrait = self.original_width < self.original_height
         self.aspect_ratio = float(self.original_width) / float(self.original_height)
@@ -27,12 +27,16 @@ class Image(Drawable):
             scaled_width = int(scaled_height * self.aspect_ratio)
         return min(float(scaled_width) / self.original_width, float(scaled_height) / self.original_height)
 
+    @classmethod
+    def to_tk_image(cls, cvimage):
+        b, g, r = cv2.split(cvimage)
+        cvimage = cv2.merge((r, g, b))
+        cvimage = ImageTk.Image.fromarray(cvimage)
+        return ImageTk.PhotoImage(image=cvimage)
+
     def draw(self, canvas: Canvas, width, height):
         scale_factor = self.calc_scale_factor(width, height)
         if math.isclose(scale_factor, .0): return
         img = cv2.resize(self.original_image, (0, 0), fx=scale_factor, fy=scale_factor)
-        b, g, r = cv2.split(img)
-        img = cv2.merge((r, g, b))
-        img = ImageTk.Image.fromarray(img)
-        self.image = ImageTk.PhotoImage(image=img)
+        self.image = self.to_tk_image(img)
         canvas.draw_image(self.image)
